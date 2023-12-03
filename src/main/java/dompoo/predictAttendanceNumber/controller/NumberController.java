50b83f7e301e460cd0106b2c483e33eaa21a9d5d
Dompoo <dlckdrms0517@gmail.com>
2023-12-03 14:05:25 +0900

    feat : 출결번호 검색 뷰 작성

--- src/main/java/dompoo/predictAttendanceNumber/controller/NumberController.java
+++ src/main/java/dompoo/predictAttendanceNumber/controller/NumberController.java
@@ -6,27 +6,34 @@ import dompoo.predictAttendanceNumber.response.NumberResponse;
 import dompoo.predictAttendanceNumber.service.NumberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class NumberController {
 
     private final NumberService numberService;
 
     @PostMapping("/attnumber/find")
-    public String getNumber(Model model, BindingResult result, @RequestParam String classNum) {
-        if (!numberService.isPresent(classNum)) {
-            result.rejectValue("class number", "NO_NUMBEr", "일치하는 출결번호가 없습니다.");
-            model.addAttribute("classNum", classNum);
-            return "display_number";
-        }
+    public String getNumber(Model model, @RequestParam String classNum) {
+//        if (!numberService.isPresent(classNum)) {
+//            result.rejectValue("class number", "NO_NUMBER", "일치하는 출결번호가 없습니다.");
+//            model.addAttribute("classNum", classNum);
+//            return "display_number";
+//        }
+        log.info("출결번호 검색, 학수번호 : {}", classNum);
 
         NumberResponse findNumber = numberService.getNumber(new NumberSearchRequest(classNum));
         model.addAttribute("findNumber", findNumber);
+        log.info("출결번호 검색 성공, 출결번호 : {}", findNumber.getNumber());
+
         return "display_number";
     }
 
